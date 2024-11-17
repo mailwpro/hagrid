b4d92f0ec1866150307707a5a81714d4bfdef2aa
Vincent Breitmoser <look@my.amazin.horse>
2024-11-17 13:43:12 +0100

    use rust-crypto instead of ring for sealed state
    Newer versions of ring are very obscure, and I couldn't figure out how
    to use its interface in a reasonable time. I used the rust-crypto
    methods instead where things were straightforward.

--- src/sealed_state.rs
+++ src/sealed_state.rs
@@ -1,64 +1,47 @@
-use ring::aead::{open_in_place, seal_in_place, Algorithm, AES_256_GCM};
-use ring::aead::{OpeningKey, SealingKey};
-use ring::digest;
-use ring::hmac;
-use ring::rand::{SecureRandom, SystemRandom};
+use aes_gcm::{
+    aead::{Aead, OsRng},
+    AeadCore, Aes256Gcm, Nonce, Key, KeyInit,
+};
+use sha2::{Sha256, Digest};
 
-// Keep these in sync, and keep the key len synced with the `private` docs as
-// well as the `KEYS_INFO` const in secure::Key.
-static ALGO: &Algorithm = &AES_256_GCM;
 const NONCE_LEN: usize = 12;
 
 pub struct SealedState {
-    sealing_key: SealingKey,
-    opening_key: OpeningKey,
+    cipher: Aes256Gcm,
 }
 
 impl SealedState {
     pub fn new(secret: &str) -> Self {
-        let salt = hmac::SigningKey::new(&digest::SHA256, b"hagrid");
-        let mut key = vec![0; 32];
-        ring::hkdf::extract_and_expand(&salt, secret.as_bytes(), b"", &mut key);
+        let mut hash = Sha256::new();
+        hash.update(b"hagrid");
+        hash.update(secret);
+        let hashed_secret = hash.finalize();
+        let key = Key::<Aes256Gcm>::from_slice(&hashed_secret);
+        let cipher = Aes256Gcm::new(&key);
 
-        let sealing_key = SealingKey::new(ALGO, key.as_ref()).expect("sealing key creation");
-        let opening_key = OpeningKey::new(ALGO, key.as_ref()).expect("sealing key creation");
-
-        SealedState {
-            sealing_key,
-            opening_key,
-        }
+        SealedState { cipher }
     }
 
     pub fn unseal(&self, data: &[u8]) -> Result<String, &'static str> {
         if data.len() < NONCE_LEN {
             return Err("invalid sealed value: too short");
         }
-        let (nonce, sealed) = data.split_at(NONCE_LEN);
-        let mut sealed_copy = sealed.to_vec();
-        let unsealed = open_in_place(&self.opening_key, nonce, &[], 0, &mut sealed_copy)
+        let (sealed, nonce) = data.split_at(data.len() - NONCE_LEN);
+        let unsealed = self.cipher.decrypt(Nonce::from_slice(nonce), sealed)
             .map_err(|_| "invalid key/nonce/value: bad seal")?;
 
-        ::std::str::from_utf8(unsealed)
+        core::str::from_utf8(&unsealed)
             .map(|s| s.to_string())
             .map_err(|_| "bad unsealed utf8")
     }
 
     pub fn seal(&self, input: &str) -> Vec<u8> {
-        let mut data;
-        let output_len = {
-            let overhead = ALGO.tag_len();
-            data = vec![0; NONCE_LEN + input.len() + overhead];
-
-            let (nonce, in_out) = data.split_at_mut(NONCE_LEN);
-            SystemRandom::new()
-                .fill(nonce)
-                .expect("couldn't random fill nonce");
-            in_out[..input.len()].copy_from_slice(input.as_bytes());
-
-            seal_in_place(&self.sealing_key, nonce, &[], in_out, overhead).expect("in-place seal")
-        };
-
-        data[..(NONCE_LEN + output_len)].to_vec()
+        let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+        let mut sealed = self.cipher
+            .encrypt(&nonce, input.as_bytes())
+            .expect("sealing works");
+        sealed.extend(nonce);
+        sealed
     }
 }
 
@@ -69,8 +52,10 @@ mod tests {
     #[test]
     fn test_encrypt_decrypt() {
         let sv = SealedState::new("swag");
-
         let sealed = sv.seal("test");
+
+        // use a different instance to make sure no internal state remains
+        let sv = SealedState::new("swag");
         let unsealed = sv.unseal(sealed.as_slice()).unwrap();
 
         assert_eq!("test", unsealed);
